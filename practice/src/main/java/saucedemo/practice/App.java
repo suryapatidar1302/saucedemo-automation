914875c6d0c16b88f22362955134f6d5cca2f6a0
Surya Patidar <suryapatidar1302@gmail.com>
2025-07-24 14:23:11 +0530

    Initial commit

---
+++ practice/src/main/java/saucedemo/practice/App.java
@@ -0,0 +1,13 @@
+package saucedemo.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
